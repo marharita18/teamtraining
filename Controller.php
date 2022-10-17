390e425738fdabd56ef9283f23e791782f7cd1b2
Denzik <denzik.kornilov@gmail.com>
2022-10-17 16:20:15 +0300

    rewrite index method

--- Controller.php
+++ Controller.php
@@ -1,11 +1,16 @@
 <?php
 
+use \Model;
 class Controller
 {
     const DEFAULT_TEMPLATE = 'template.php';
+    public $model;
+    public function __construct(){
+        $this->model = new Model;
+    }
 
-    public function index(string $template = self::DEFAULT_TEMPLATE, array $data = []){
-        extract($data);
-        include_once  $template;
+    public function index(string $template = self::DEFAULT_TEMPLATE){
+        $users = $this->model->all();
+        include_once $template;
     }
 }
